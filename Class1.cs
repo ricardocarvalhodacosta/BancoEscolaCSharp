c98194c1615c64f9e22b8e4fb8b3291c24cd4ee9
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-04-21 20:41:39 -0300

    Iniciando o Projeto de Banco de Dados Escolar
    Iniciado o projeto de reescrever os códigos do meu banco de dados para escolas. Neste projeto irei mudar algo que não tinha no outro, no caso, alterar dados de um aluno em especifico quando necessário.

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoEscola
+{
+    internal class Menu
+    {
+        public char menu { get; set; }
+        public int op { get; set; }
+        public string leitura { get; set; }
+
+        public void SelectClass()
+        {
+            Console.WriteLine("Olá, eu sou o banco de dados da Escola,");
+            Console.WriteLine("e estou aqui para ajudar!");
+            Console.WriteLine("Antes de começarmos, me diga:");
+            Console.WriteLine("Qual classe você gostaria de acessar?");
+            Console.WriteLine();
+            Console.WriteLine("Classe A, Classe B ou Classe C?");
+            Console.WriteLine();
+            Console.Write("Por favor, digite a letra da Classe: ");
+        }
+
+        public void SelectFuncion()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ótimo, você escolheu a Classe " + menu + ", agora me diga, o que iremos fazer hoje?");
+            Console.WriteLine();
+            Console.WriteLine("1. Adicionar um novo Aluno;");
+            Console.WriteLine("2. Alterar os Dados de um Aluno;");
+            Console.WriteLine();
+        }
+    }
+
+    internal class Dados
+    {
+        public string nome { get; set; }
+        public float nota1 { get; set; }
+        public float nota2 { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using BancoEscola;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.SelectClass();
+        menu.menu = char.Parse(Console.ReadLine().ToUpper());
+        menu.SelectFuncion();
+        menu.op = int.Parse(Console.ReadLine());
+
+        if (menu.op == 1)
+        {
+            switch (menu.menu)
+            {
+                case 'A':
+                    Console.WriteLine("Dados dos Alunos da Classe A");
+                    break;
+                case 'B':
+                    Console.WriteLine("Dados dos Alunos da Classe B");
+                    break;
+                case 'C':
+                    Console.WriteLine("Dados dos Alunos da Classe C");
+                    break;
+                default: Console.WriteLine("Dados invalidos");
+                    break;
+            }
+        }
+    }
+}
